3647401250f8af41de53c5492bb1e1b47ba59215
CARLA <carla_riva@estudante.sesisenai.org.br>
2024-06-25 15:13:25 -0300

    RESOLUÇÃO EXERCÍCIO 06

---
+++ LISTA_01/EXERCÍCIO_06/indez.js
@@ -0,0 +1,21 @@
+
+let botao = document.getElementById('botao')
+
+botao.addEventListener ('click', function() {
+    console.log('aa')
+    let newCor = generateColor()
+    document.body.style.backgroundColor =  newCor
+    
+})
+
+function generateColor () {
+    const hexNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    let cor = '#'
+    for (let i = 0; i < 6; i++) {
+    console.log('a')
+        cor+= hexNumbers [Math.floor(Math.random() * 16)]
+    }
+
+return cor
+
+}
